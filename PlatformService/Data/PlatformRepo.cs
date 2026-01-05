72282d5a21c8d2f8b2afc2441d6facc2ab596b4a
Hussam82 <hussamali30@gmail.com>
2026-01-05 14:11:04 +0200

    Added the initial skeleton and the first controller

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,34 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
